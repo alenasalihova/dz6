90d1ef62a1922b77e132277288eb220b2b1de822
Salihova Alena <alexx9202@gmail.com>
2024-01-07 20:36:48 +0200

    second

--- src/comp/Content1.js
+++ src/comp/Content1.js
@@ -1,15 +1,9 @@
 import React, { Component } from 'react';
 
-import { Users } from './Users';
+
 
 export class Content1 extends Component {
-state = (users: users);
-isShowUser = (user) => {
-    user.isShow = !user.isShow;
-    this.setState((st) => ({users: st.users.map(userL => {
-        if (userL.id == user.id) return user
-    })}))
-}
+
   render() {
 
     return (
@@ -26,7 +20,7 @@ isShowUser = (user) => {
             Excepteur sint occaecat cupidatat non 
             proident, sunt in culpa qui officia deserunt 
             mollit anim id est laborum.</p>
-        
+
       </div>
     );
   }
